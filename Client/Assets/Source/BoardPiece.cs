cba3f4c51efa5330fcf529af9423bfeb830b5a40
Neil Richardson <richyneil@gmail.com>
2013-12-14 19:10:13 +1100

    materials, board piece connectivity.

--- Client/Assets/Source/BoardPiece.cs
+++ Client/Assets/Source/BoardPiece.cs
@@ -3,12 +3,44 @@ using System.Collections;
 
 public class BoardPiece : MonoBehaviour
 {
+	[System.Flags]
+	public enum Connectivity
+	{
+		Top = 0x1,
+		Bottom = 0x2,
+		Left = 0x4,
+		Right = 0x8
+	}
+	
 	private int _id = 0;
+	
+	[EnumFlags]
+	public Connectivity TopEdge;
+
+	[EnumFlags]
+	public Connectivity BottomEdge;
+	
+	[EnumFlags]
+	public Connectivity LeftEdge;
+	
+	[EnumFlags]
+	public Connectivity RightEdge;
+	
+	
+	private Connectivity[] _connectivity;
+	
 	
 	// Use this for initialization
 	void Start ()
 	{
-	
+		// Grab public stuff, put in indexable container.
+		_connectivity = new Connectivity[]
+		{
+			TopEdge,
+			BottomEdge,
+			LeftEdge,
+			RightEdge
+		};
 	}
 	
 	// Update is called once per frame
---
+++ Client/Assets/Source/EnumFlagsAttribute.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+using System;
+
+public class EnumFlagsAttribute : PropertyAttribute
+{
+    public EnumFlagsAttribute() { }
+}
