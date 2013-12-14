bdb063052c02f2ec6fd32f98026e702703c7d3c5
Neil Richardson <richyneil@gmail.com>
2013-12-14 15:40:10 +1100

    Initial unity project commit with basic game scene setup

---
+++ Client/Assets/Source/BoardPiece.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardPiece : MonoBehaviour
+{
+	/// <summary>
+	/// Is the board piece fixed? I.e. can't be moved?
+	/// </summary> 
+	public bool IsFixed = false;
+
+	
+	private int _x = 0;
+	private int _y = 0;
+	
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+	
+	// Setup piece with play area and position.
+	public void SetupPiece( BoardPlayArea playArea, int X, int Y )
+	{
+		// Parent transform.
+		if( playArea != null )
+		{
+			transform.parent = playArea.transform;
+		}
+		
+		// X & Y.
+		_x = X;
+		_y = Y;
+		
+		// Setup position.
+		transform.localPosition = new Vector3( (float)X, 0.0f, (float)Y );
+	}
+	
+	
+}
---
+++ Client/Assets/Source/BoardPlayArea.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardPlayArea : MonoBehaviour 
+{
+	/// <summary>
+	/// The template piece.
+	/// </summary>
+	public GameObject TemplatePiece;
+	
+	
+	
+	private 
+	
+
+	// Use this for initialization
+	void Start ()
+	{
+		// Clone all board pieces.
+		for( int Y = -3; Y <= 3; ++Y )
+		{
+			for( int X = -3; X <= 3; ++X )
+			{
+				var newPiece = Object.Instantiate( TemplatePiece ) as GameObject;
+				var boardPiece = newPiece.GetComponent< BoardPiece >();
+				boardPiece.SetupPiece( this, X, Y );
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
