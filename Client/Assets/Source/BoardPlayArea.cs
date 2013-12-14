8b601d03c58962fb27f1a9c810cbb2e7b12d9600
Neil Richardson <richyneil@gmail.com>
2013-12-14 21:40:06 +1100

    shuffling of tiles.

--- Client/Assets/Source/BoardPlayArea.cs
+++ Client/Assets/Source/BoardPlayArea.cs
@@ -29,17 +29,29 @@ public class BoardPlayArea : MonoBehaviour
 	void Start ()
 	{
 		// Create board pieces.
-		for( int i = 0; i < 60; ++i )
+		foreach( var templateBoardPiece in TemplateBoardPieces)
 		{
-			var boardPieceObject = Object.Instantiate( TemplatePiece.gameObject ) as GameObject;
-			var boardPiece = boardPieceObject.GetComponent< BoardPiece >();
-			_boardPiecePile.Add( boardPiece );
-			boardPiece.SetupPiece( this, i );
+			for( int i = 0; i < templateBoardPiece.TotalInDeck; ++i )
+			{
+				var boardPieceObject = Object.Instantiate( templateBoardPiece.gameObject ) as GameObject;
+				var boardPiece = boardPieceObject.GetComponent< BoardPiece >();
+				_boardPiecePile.Add( boardPiece );
+				boardPiece.SetupPiece( this, i );
 			
-			var position = GetPilePosition();
-			boardPiece.transform.parent = transform;
-			boardPiece.transform.localPosition = position;			
-			boardPiece.transform.localRotation = _pileRotation;
+				var position = GetPilePosition();
+				boardPiece.transform.parent = transform;
+				boardPiece.transform.localPosition = position;			
+				boardPiece.transform.localRotation = _pileRotation;
+			}
+		}
+		
+		// Shuffle pieces.
+		Random.seed = 0; // hard code seed for now.
+		for( int i = 0; i < _boardPiecePile.Count * 4; ++i )
+		{
+			var randomIdx = Random.Range( 0, _boardPiecePile.Count);
+			_boardPiecePile.Insert( randomIdx, _boardPiecePile[0] );
+			_boardPiecePile.RemoveAt(0);
 		}
 		
 		// Create board piece play locations.
@@ -215,7 +227,7 @@ public class BoardPlayArea : MonoBehaviour
 				objectMover = boardPieceObject.GetComponent< ObjectMover >();
 				objectMover.Move(position, Quaternion.identity, 1.0f, null);
 			}
-			else if( y == -1 ) // Top
+			else if( y == -1 ) // Bottom
 			{
 				for( int i = Size - 1; i >= 0; --i )
 				{
@@ -247,9 +259,9 @@ public class BoardPlayArea : MonoBehaviour
 				_boardPieceField[x][0] = boardPieceObject;
 				position = GetPiecePosition(x, 0);
 				objectMover = boardPieceObject.GetComponent< ObjectMover >();
-				objectMover.Move(position, _pileRotation, 1.0f, null);
+				objectMover.Move(position, Quaternion.identity, 1.0f, null);
 			}
-			else if( y == Size ) // Bottom
+			else if( y == Size ) // Top
 			{
 				for( int i = 0; i < Size; ++i )
 				{
