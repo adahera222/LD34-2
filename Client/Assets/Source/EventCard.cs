7ad34b93f5af40cc2b5b02706b6cbdd1c17dbca9
Neil Richardson <richyneil@gmail.com>
2013-12-15 19:08:11 +1100

    Event text!

--- Client/Assets/Source/EventCard.cs
+++ Client/Assets/Source/EventCard.cs
@@ -6,44 +6,46 @@ public class EventCard : MonoBehaviour
 {
 	public List<EventPiece> EventPieces = new List<EventPiece>();
 	
-	public List<string> EventStrings = new List<string>()
+	public List<TextMesh> TextBits = new List<TextMesh>();
+	
+	private List<string> _eventStrings = new List<string>()
 	{
 		"Building of the Great Pyramid.",
-		"Work on the Great Wall of China begins.",
+		"Work on the Great Wall of China\nbegins.",
 		"Roger Bacon invents gunpowder.",
 		"English face Black Death Plague.",
 		"Death of Napolean.",
 		"Slavery absolished in USA.",
-		"South Africa emerged from aparted regime with Nelson Mandela as its president.",
+		"South Africa emerged from aparted\nregime with NelsonMandela\nas its president.",
 		"Death of Sir Winston Churchill.",
-		"First artificial satellite launched by Russian.",
+		"First artificial satellite\nlaunched by Russian.",
 		"World War II begins.",
-		"Hitler becomes the Chanceller or Germany.",
+		"Hitler becomes the Chanceller of\nGermany.",
 		"Beginning of World War I.",
 		"Death of Nelson Mandela.",
-		"Euro becomes the official currency of 12 European countries.",
-		"Cook' discovery of New South Wales, Australia.",
+		"Euro becomes the official currency\nof 12 European countries.",
+		"Cook' discovery of New South Wales,\nAustralia.",
 		"Flinders circumnavigates Australia.",
-		"Australian Prime Minister Harold Holt Disappears.",
-		"English Civil War: The Battle of Alton takes place in Hampshire.",
-		"Beatles World Tour begins in Copenhagen, Denmark.",
+		"Australian Prime Minister\nHarold Holt Disappears.",
+		"English Civil War: The Battle\nof Alton takes place in Hampshire.",
+		"Beatles World Tour begins in\nCopenhagen, Denmark.",
 		"7810BC....date 20.",
-		"US Female Figure Skating championship won by Peggy Fleming.",
-		"Buddy Holly records \"Blue Days Black Night\" in Nashville.",
-		"1st English Parliament called into session by Earl of Leicester.",
-		"1st muse store in America opens.",
-		"\"Star Trek II: The Wrath of Khan\" released in the USA.",
-		"Boston Red Sox beat Brooklyn Dodgers 4 games to 1 in 13th World Series.",
+		"US Female Figure Skating\nchampionship won by Peggy Fleming.",
+		"Buddy Holly records \"Blue Days\nBlack Night\" in Nashville.",
+		"1st English Parliament called\ninto session by Earl of Leicester.",
+		"1st music store in America opens.",
+		"\"Star Trek II: The Wrath of Khan\"\nreleased in the USA.",
+		"Boston Red Sox beat Brooklyn Dodgers\n4 games to 1 in 13th\nWorld Series.",
 		"Belgium disbands salt tax.",
-		"Interplanetary Hunger Commission meets for the first time to fight universal poverty.",
-		"Time travel invented. A new era, ATT, begins.",
-		"Largest known Unobtanium mining operation begins on Xenon-12.",
-		"Ludum Dare community is now the largest Earth colony.",
-		"Venus and Mars sign treaty over Earth ownership.",
-		"Pluto is officially redeclared a planet.",
-		"First known casualty of interplanetary patent war.",
-		"Reality television is banned on 27/53 Human controlled planets.",
-		"One year since first to-be time travellers teamed to safeguard our future."
+		"Interplanetary Hunger Commission\nmeets for the first time to\nfight universal poverty.",
+		"Time travel invented. A new era,\nATT, begins.",
+		"Largest known Unobtanium mining\noperation begins on Xenon-12.",
+		"Ludum Dare community is now the\nlargest Earth colony.",
+		"Venus and Mars sign treaty over\nEarth ownership.",
+		"Pluto is officially redeclared a\nplanet.",
+		"First known casualty of\ninterplanetary patent war.",
+		"Reality television is banned on\n27/53 Human controlled planets.",
+		"One year since first to-be time\ntravellers teamed to safeguard\nour future."
 	};
 	
 	// Use this for initialization
@@ -64,6 +66,8 @@ public class EventCard : MonoBehaviour
 		{
 			var eventPiece = EventPieces[i];
 			eventPiece.SetTexture( eventTextures[events[i]], events[i] );		
+			
+			TextBits[i].text = _eventStrings[ events[i] ];
 		}
 	}
 	
